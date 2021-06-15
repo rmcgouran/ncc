6c78162a33b4faae6dd9dec3f3ad357797907253
Ryan <ryan.mcgouran2018@my.ntu.ac.uk>
2021-06-15 13:17:13 +0100

    Changed to Covid FAQ API
    Successful API call setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import Axios from 'axios';
+import Axios from 'axios'; //helps interface with APIs
 import './App.css';
 
 function App() {
 
-  const commonHealth = () => {
-    Axios.get("https://api.nhs.uk/common-health-questions/travel-health?",
+  const getFaqs = () => {
+    Axios.get("https://api.nhs.uk/conditions/coronavirus-covid-19/faqs",
       {
         "headers": {
           "subscription-key": "30dad3f05e124ca2832649645958f434"
         }
       }).then((response) => {
-        console.log(response);
-      });
+        console.log("Covid FAQs recieved");
+        console.log(response.data.mainEntity);
+      })
   };
 
   return (
     <div className="App">
-      <button onClick={commonHealth}>get travel health advice</button>
+      <h1>Covid FAQ</h1>
+      <button onClick={getFaqs}>Request covid faqs</button>
     </div>
   );
 }
