c27dac0d09cecb8028092f6380401d14b4c0147e
Ryan <ryan.mcgouran2018@my.ntu.ac.uk>
2021-06-15 14:00:21 +0100

    Working display of API data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import Axios from 'axios'; //helps interface with APIs
+import React, { useState } from "react";
+import Axios from "axios";
 import './App.css';
 
 function App() {
-
+  const [data, setData] = useState(null);
   const getFaqs = () => {
-    Axios.get("https://api.nhs.uk/conditions/coronavirus-covid-19/faqs",
-      {
-        "headers": {
-          "subscription-key": "30dad3f05e124ca2832649645958f434"
-        }
-      }).then((response) => {
-        console.log("Covid FAQs recieved");
-        console.log(response.data.mainEntity);
-      })
+    Axios.get("https://api.nhs.uk/conditions/coronavirus-covid-19/faqs", {
+      headers: {
+        "subscription-key": "30dad3f05e124ca2832649645958f434"
+      }
+    }).then((response) => {
+      console.log(response.data.mainEntity);
+      setData(response.data.mainEntity[3]);
+    });
   };
 
   return (
     <div className="App">
-      <h1>Covid FAQ</h1>
       <button onClick={getFaqs}>Request covid faqs</button>
+      <h1>Covid FAQ</h1>
+      <h2>{data?.name}</h2>
+      <p>{data?.acceptedAnswer.text}</p>
+      <a href={data?.sameAs}>Visit the NHS site</a>
     </div>
   );
 }
+
 export default App;
