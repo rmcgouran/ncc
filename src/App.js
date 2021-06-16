86a14945e74ff39d31391cd8f6ee7918939d451f
Ryan <ryan.mcgouran2018@my.ntu.ac.uk>
2021-06-16 12:48:57 +0100

    Changed to useEffect
    Data is now fetched from the api when the page loads

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
-  const getFaqs = () => {
+
+  // Attempt to fetch data on page render 
+  // function componentDidMount() {
+  //   Axios.get("https://api.nhs.uk/conditions/coronavirus-covid-19/faqs", {
+  //     headers: {
+  //       "subscription-key": "30dad3f05e124ca2832649645958f434"
+  //     }
+  //   }).then((response) => {
+  //     console.log(response.data.mainEntity);
+  //     setData(response.data.mainEntity[1]);
+  //   });
+  // }
+
+  useEffect(() => {
     Axios.get("https://api.nhs.uk/conditions/coronavirus-covid-19/faqs", {
       headers: {
         "subscription-key": "30dad3f05e124ca2832649645958f434"
       }
     }).then((response) => {
       console.log(response.data.mainEntity);
-      setData(response.data.mainEntity[3]);
+      setData(response.data.mainEntity[0]);
     });
-  };
+  });
 
   return (
+
     <div className="App">
-      <button onClick={getFaqs}>Request covid faqs</button>
+      {/* {componentDidMount()} Caused a fetch loop - removed*/}
       <h1>Covid FAQ</h1>
       <h2>{data?.name}</h2>
       <p>{data?.acceptedAnswer.text}</p>
       <a href={data?.sameAs}>Visit the NHS site</a>
+      {/* {data.map((data, i) => (<h2>{data.name}</h2>))} */}
     </div>
   );
 }
