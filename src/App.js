2db4f26ec4787198f8eefc55d6e47f4b28903893
Ryan <ryan.mcgouran2018@my.ntu.ac.uk>
2021-06-17 03:58:54 +0100

    Added more styling, added NHS Attribution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import './App.css';
+import logo from './logo.png';
 
 function App() {
-  const [data, setData] = useState([]);
 
-  useEffect(() => { // API data now loaded when the page loads 
+  // Call the API and set the useState.
+  const [data, setData] = useState([]);
+  useEffect(() => { // API data loaded on page load.
     Axios.get("https://api.nhs.uk/conditions/coronavirus-covid-19/faqs", {
       headers: {
         "subscription-key": "30dad3f05e124ca2832649645958f434"
@@ -20,16 +22,20 @@ function App() {
     <div className="App">
       {/* <button onClick={getFaqs}>Request Covid-19 FAQs</button> initially used to load data */}
       <header>
-        <h1 className="header">Covid FAQ</h1>
+        <div className="container">
+          <h1 className="logo">Covid-19 FAQs</h1>
+        </div>
       </header>
-      <body>
+      <body className="info">
         {data.map(item => (
           <>
-            <h2>{item.name ?? 'Name'}</h2>
-            <p>{item.acceptedAnswer.text ?? 'Answer'}</p>
-            <a href={item.sameAs}>Visit this page on the official NHS site</a>
+            <h2 className="text">{item.name ?? 'Name'}</h2>
+            <p className="text">{item.acceptedAnswer.text ?? 'Answer'}</p>
+            <a className="text" href={item.sameAs} target="_blank">Visit this page on the official NHS site</a>
           </>
-        ))}</body>
+        ))}
+      </body>
+      <img className="nhs" src={logo} alt="NHS Attribution" />
     </div>
   );
 }
