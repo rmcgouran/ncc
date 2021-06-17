36e0f0361fbbfa3f3c9b785ed8e6dd9db5974902
Ryan <ryan.mcgouran2018@my.ntu.ac.uk>
2021-06-17 03:10:57 +0100

    Some CSS added, slight code refactor

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
+  useEffect(() => { // API data now loaded when the page loads 
     Axios.get("https://api.nhs.uk/conditions/coronavirus-covid-19/faqs", {
       headers: {
         "subscription-key": "30dad3f05e124ca2832649645958f434"
@@ -18,17 +18,18 @@ function App() {
 
   return (
     <div className="App">
-      {/* <button onClick={getFaqs}>Request Covid-19 FAQs</button> */}
-      <h1>Covid FAQ</h1>
-      {data.map(item => (
-        <>
-          <h2>{item?.name ?? 'Name'}</h2>
-          <p>{item?.acceptedAnswer.text ?? 'Answer'}</p>
-          {item?.sameAs && (
-            <a href={item?.sameAs}>Visit the NHS site</a>
-          )}
-        </>
-      ))}
+      {/* <button onClick={getFaqs}>Request Covid-19 FAQs</button> initially used to load data */}
+      <header>
+        <h1 className="header">Covid FAQ</h1>
+      </header>
+      <body>
+        {data.map(item => (
+          <>
+            <h2>{item.name ?? 'Name'}</h2>
+            <p>{item.acceptedAnswer.text ?? 'Answer'}</p>
+            <a href={item.sameAs}>Visit this page on the official NHS site</a>
+          </>
+        ))}</body>
     </div>
   );
 }
